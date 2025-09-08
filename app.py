30a5ecca9240b398900eb280933fb6e38074783c
yangzhiyuan <yangzhiyuan@inftech.ai>
2025-09-08 11:49:08 +0800

    feature:add port config

--- app.py
+++ app.py
@@ -313,4 +313,7 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    # Load environment variables
+    load_dotenv()
+    PORT = int(os.getenv("PORT",8000))
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
